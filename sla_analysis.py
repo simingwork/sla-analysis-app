7c2996aa374534fac87998e9409991349e025b49
simingwork <simingwork622@gmail.com>
2026-03-18 10:03:36 -0700

    Update sla_analysis.py

--- sla_analysis.py
+++ sla_analysis.py
@@ -308,7 +308,7 @@ def run_analysis(
     # Update AE SLA start time for customhouse time after 9pm
     df["SLA关配交接时间"] = df["关配交接时间"]
     
-    mask_late = (df["客户"] == "AE") & ~df["集配站"].isin("HUB_LAX_PHX", "HUB_LAX_LAS") & df["关配交接时间"].notna() & (df["关配交接时间"].dt.hour >= 21)
+    mask_late = (df["客户"] == "AE") & ~df["集配站"].isin(["HUB_LAX_PHX", "HUB_LAX_LAS"]) & df["关配交接时间"].notna() & (df["关配交接时间"].dt.hour >= 21)
     df.loc[mask_late, "SLA关配交接时间"] = (
         df.loc[mask_late, "关配交接时间"].dt.normalize() + pd.Timedelta(days=1)
     )
