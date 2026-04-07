c0cdeeb132edd27974d3fb421978e637a983ce1a
simingwork <simingwork622@gmail.com>
2026-04-07 14:53:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from io import BytesIO
 from datetime import datetime, date, time
 
-from client_sla_analysis import run_analysis  
+from client_sla_analysis import run_analysis
 from cainiao_sla_analysis import run_cainiao_analysis
 
 st.set_page_config(page_title="客户SLA未达分析", layout="wide")
